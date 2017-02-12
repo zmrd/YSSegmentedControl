54c765e1cbd7013a45e9e2faef4641d51572bce0
Stepan Votava <stepan.votava@inloop.eu>
2017-02-12 17:48:34 +0100

    Open struct appearance

--- YSSegmentedControl/YSSegmentedControl/YSSegmentedControl.swift
+++ YSSegmentedControl/YSSegmentedControl/YSSegmentedControl.swift
@@ -10,7 +10,7 @@ import UIKit
 
 // MARK: - Appearance
 
-public struct YSSegmentedControlAppearance {
+open struct YSSegmentedControlAppearance {
     public var backgroundColor: UIColor
     public var selectedBackgroundColor: UIColor
     public var textColor: UIColor
